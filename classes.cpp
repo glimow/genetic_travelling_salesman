84fe7324998de19e0cc6cf7698c701e3bdd200ae
Tristan <tristan.kalos@ensta.fr>
2017-02-23 10:36:41 -0500

    added population sorting

--- classes.cpp
+++ classes.cpp
@@ -1,41 +1,50 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
+/*
 class Individu{
-  void mutation(); // fait muter l'individu
-  float adaptation(); // renvoie la valeur de la fonction d'adaptation pour cet individu
+  public:
+    void mutation(); // fait muter l'individu
+    float adaptation() const; // renvoie la valeur de la fonction d'adaptation pour cet individu
+};*/
+
+template <class Individu>  bool compareIndividus(const Individu& g, const Individu& d){
+  return g.adaptation() < d.adaptation();
 };
 
-Individu operator*(Individu const& a, Individu const& b){};
 
-
-class Population{
+template <class Individu> class Population{
 private:
-  vector<Individu> pop; //Individus stockés dans la population, vecteur d'individus
 public:
+  vector<Individu> pop; //Individus stockés dans la population, vecteur d'individus
+  Population selectBestIndv(int q){
+    sort(this->pop.begin(), this->pop.end(), compareIndividus<Individu>);
+    vector<Individu> nouvellePop(this->pop.begin(),this->pop.begin()+q);
+    Population resultPop;
+    resultPop.pop = nouvellePop;
+    return resultPop;
+  };
+
   Population selection(int p, int type); //retourne une population de p individus
   // sélectionnés par la méthode type
   void reproduction(); //fait se reproduire entre eux les individus de la Population de p éléments
-  Population selectBestIndv(int q);
 };
 
-class Chemin : public Individu{
+//class individu:
+class Chemin
+{
   public:
     vector<int> path;
     void mutation(){
       //Index de la mutation flip
       int mutationIndex = rand() % this->path.size() - 1;
       //Création des deux sous vecteurs que l'on va flipper
-      vector<int> beginning(this->path.begin(), this->path.begin() + mutationIndex);
-      vector<int> ending(this->path.begin() + mutationIndex + 1, this->path.end());
-      //flip des vecteurs
-      ending.insert( ending.end(), beginning.begin(), beginning.end() );
-      this->path = ending;
+      swap(this->path[mutationIndex],this->path[mutationIndex+1]);
     };
 
-    float adaptation(){
+    float adaptation() const{
       float adaptation = 0; // l'adpatation est l'inverse de la distance du chemin
       for(int i = 1; i < this->path.size(); i++){
         //somme des distances entre les villes

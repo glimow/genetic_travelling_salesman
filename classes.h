b3272016a023d6256d7c405f171523ede9926edf
Tristan Kalos <tristan.kalos@live.fr>
2017-12-22 00:34:36 +0100

    Update classes.h

--- classes.h
+++ classes.h
@@ -4,21 +4,28 @@
 using namespace std;
 
 class Individu{
+  
   void mutation(); // fait muter l'individu
   float adaptation(); // renvoie la valeur de la fonction d'adaptation pour cet individu
+
 };
 
 Individu operator*(Individu const& a, Individu const& b);
 
 
 class Population{
+
 private:
+  
   vector<Individu> pop; //Individus stockés dans la population, vecteur d'individus
+
 public:
+
   Population selection(int p, int type); //retourne une population de p individus
   // sélectionnés par la méthode type
   void reproduction(); //fait se reproduire entre eux les individus de la Population de p éléments
   Population selectBestIndv(int q);
+
 };
 
 
